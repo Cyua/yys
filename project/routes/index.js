1940d5275a3197ad63a8402f437dda7db42d006d
Cyua <466764008@qq.com>
2016-10-08 22:36:26 +0800

    .

--- project/routes/index.js
+++ project/routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Cyua' });
+});
+
+
+router.post('/request/position', function(req, res){
+	var name = req.body.name;
+	console.log(name);
+	res.sendStatus(200);
 });
 
 module.exports = router;
