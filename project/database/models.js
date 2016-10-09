faadf021bce8fb39467e4fa111f649899e8d0f04
Cyua <466764008@qq.com>
2016-10-10 00:12:04 +0800

    add clue

--- project/database/models.js
+++ project/database/models.js
@@ -5,5 +5,8 @@ module.exports={
 		position: 	[
 			String
 		],
+		clue: 		[
+			String
+		],
 	},
 };
--- project/routes/data.js
+++ project/routes/data.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/position', function(req, res){
-	return;
 	var name = req.body.name;
 	var pinyin = req.body.pinyin;
 	var pos = req.body.position;
@@ -10,7 +9,8 @@ router.post('/position', function(req, res){
 	var data = {
 		'name':name,
 		'pinyin':pinyin,
-		'position':pos
+		'position':pos,
+		'clue':[],
 	};
 	var record = new Shikigami(data);
 	record.save(function(err){
@@ -18,6 +18,25 @@ router.post('/position', function(req, res){
 			console.log(err);
 		else
 			res.sendStatus(200);
+	});
+});
+
+
+router.post('/clue', function(req, res){
+	var name = req.body.name;
+	var clue = req.body.clue;
+	var Shikigami = global.dbHandle.getModel('shikigamis');
+	Shikigami.findOne({'name':name}, function(err, doc){
+		if(err){
+			console.log(err);
+			res.sendStatus(500);
+		}else if(!doc){
+			res.sendStatus(404);
+		}else{
+			doc.clue = clue;
+			doc.save();
+			res.sendStatus(200);
+		}
 	});
 });
 
